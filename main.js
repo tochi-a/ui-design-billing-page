3a06b3a55901fee5fe74a45f6bf1789e058814e6
tochi_a <106175827+tochi-a@users.noreply.github.com>
2022-08-26 09:16:59 +0100

    added more content to repo

---
+++ main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("overlay-nav").style.width = "60%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("overlay-nav").style.width = "0%";
+    document.body.style.backgroundColor = "white";
+}
